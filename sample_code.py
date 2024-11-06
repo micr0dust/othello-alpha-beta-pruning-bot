532eac4bd595696173513eb3bcf36161a3effcaa
micr0dust <wuilliamolgo104286@gmail.com>
2024-11-06 19:01:24 +0800

    origin

---
+++ sample_code.py
@@ -0,0 +1,10 @@
+from AIGamePlatform import Othello
+from othello.bots.Random import BOT
+
+app=Othello() # 和平台建立WebSocket連線
+bot=BOT() # 建立隨機bot
+
+@app.competition(competition_id='test') # 競賽ID
+def _callback_(board, color): # 當需要走步會收到盤面及我方棋種
+    return bot.getAction(board, color) # bot回傳落子座標
+
