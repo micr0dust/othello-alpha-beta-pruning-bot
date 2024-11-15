45f98cd60e9a87ddddaca004729d56666505febd
micr0dust <wuilliamolgo104286@gmail.com>
2024-11-15 13:23:01 +0800

    gitignore

--- self_test.py
+++ self_test.py
@@ -1,5 +1,5 @@
 from othello.bots.Random import BOT as black_bot
-from othello.bots.Alpha_Beta import BOT as white_bot
+from othello.bots.alpha_beta import BOT as white_bot
 from othello.OthelloGame import OthelloGame
 
 black=black_bot() # 建立隨機bot
